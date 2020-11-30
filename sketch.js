c3516b5b2168835910940991887609d4afad94f9
Explothing07 <69224824+Explothing07@users.noreply.github.com>
2020-11-29 21:08:21 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var ground;
+
+var divisionHeight = 300;
+
+function setup() {
+  engine = Engine.create();
+  world = engine.world;
+
+  createCanvas(480,800);
+  //createSprite(400, 200, 50, 50);
+  
+  ground = new Ground(240,798,480,20);
+
+  //divisions
+  for (var i = 0; i <= 800; i = i + 80){
+    divisions.push(new Division(i, 700, 10, divisionHeight));
+  }
+
+  //plinkos
+  for (var j = 40; j <= width; j = j + 50){
+    plinkos.push(new Plinko(j, 175));
+  }
+  for (var j = 15; j <= width - 10; j = j + 50){
+    plinkos.push(new Plinko(j, 275));
+  }
+  for (var j = 40; j <= width; j = j + 50){
+    plinkos.push(new Plinko(j,375));
+  }
+  for (var j = 15; j <= width - 10; j = j + 50){
+    plinkos.push(new Plinko(j, 475));
+  }
+}
+
+function draw() {
+  Engine.update(engine);
+  background(0,0,0);
+
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
+  
+  //displaying divisions
+  for (var k = 0; k < divisions.length; k++){
+    divisions[k].display();
+  }
+
+  //displaying plinkos
+  for (var j = 0; j < plinkos.length; j++){
+    plinkos[j].display();
+  }
+
+  //displaying particles
+  for (var i = 0; i < particles.length; i++){
+    particles[i].display();
+  }
+
+  ground.display();
+
+  drawSprites();
+}
